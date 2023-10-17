8338aadaef699363e47d6fef0cf870285e0711d4
lee hyun woo <kaway0418@naver.com>
2023-10-17 21:22:21 +0900

    10/18

---
+++ step5/practice1/src/main/java/com/example/test/repository/search/BoardSearch.java
@@ -0,0 +1,13 @@
+package com.example.test.repository.search;
+
+import com.example.test.domain.Board;
+import com.example.test.dto.BoardListReplyCountDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BoardSearch {
+
+    Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
+
+    Page<BoardListReplyCountDTO> boardOfReply(String[] types,String keyword, Pageable pageable);
+}
