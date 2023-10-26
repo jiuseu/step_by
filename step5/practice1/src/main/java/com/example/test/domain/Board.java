bc85035caa4ad720688390cef84f8256118527ce
lee hyun woo <kaway0418@naver.com>
2023-10-26 22:06:24 +0900

    10/26

--- step5/practice1/src/main/java/com/example/test/domain/Board.java
+++ step5/practice1/src/main/java/com/example/test/domain/Board.java
@@ -2,6 +2,7 @@ package com.example.test.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -30,6 +31,7 @@ public class Board extends BaseEntity{
                cascade = CascadeType.ALL,
                orphanRemoval = true)
     @Builder.Default
+    @BatchSize(size= 20)
     private Set<BoardImage> imageSet = new HashSet<>();
 
     public void Change(String title, String content){
