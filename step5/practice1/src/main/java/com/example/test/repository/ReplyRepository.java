bc2dd3c1719e945a6c6cf5e10ca1c0497c866fab
lee hyun woo <kaway0418@naver.com>
2023-10-30 23:08:14 +0900

    10/30

--- step5/practice1/src/main/java/com/example/test/repository/ReplyRepository.java
+++ step5/practice1/src/main/java/com/example/test/repository/ReplyRepository.java
@@ -10,4 +10,6 @@ public interface ReplyRepository extends JpaRepository<Reply,Long> {
 
    @Query("select r from Reply r where r.board.bno = :bno")
     Page<Reply> list(Long bno,Pageable pageable);
+
+   void deleteByBoard_bno(Long bno);
 }
