4676c691b1793ad6ee1f31b8b0df4ddca53993b6
lee hyun woo <kaway0418@naver.com>
2023-11-05 22:37:20 +0900

    11/5 BoardRepository 수정

--- step5/practice1/src/main/java/com/example/test/repository/BoardRepository.java
+++ step5/practice1/src/main/java/com/example/test/repository/BoardRepository.java
@@ -2,11 +2,15 @@ package com.example.test.repository;
 
 import com.example.test.domain.Board;
 import com.example.test.repository.search.BoardSearch;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
 
-//    @Query("select b from board b where b.bno=:bno")
-//    Page<Board>
+    @EntityGraph(attributePaths = {"imageSet"})
+    @Query("select b from Board b where b.bno=:bno")
+    Optional<Board> findByIdWithImages(Long bno);
 }
